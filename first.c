93de93dcd8ec19d5a83ac97ffdf2bc76f09b5c17
nowshad89 <nowshadmahmud89@gmail.com>
2023-04-04 02:20:42 +0600

    first module done

--- first.c
+++ first.c
@@ -46,17 +46,50 @@
 // = assignment operator
 // format specifier % used as %d, %f, %c for printing certian type of data
 
-#include<stdio.h>
-int main()
-{
-    int rahim = 10;
-    float karim = 10.34343434;
-    char abul = 'k';
-    printf("%0.3f \n", karim);
-    printf("%d \n", rahim);
-    printf("%c", abul);
+// #include<stdio.h>
+// int main()
+// {
+//     int rahim = 10;
+//     float karim = 10.34343434;
+//     char abul = 'k';
+//     printf("%0.3f \n", karim);
+//     printf("%d \n", rahim);
+//     printf("%c", abul);
 
-}
+// }
 
+// why we need to take input(scanf)-----------
+// & is used to locate, it is named adress of
+// #include<stdio.h>
+// int main()
+// {
+//     // int rahim, karim;
+//     // scanf("%d %d", &rahim, &karim);
+//     // printf("%d %d", rahim, karim);
 
+//     int a, b;
+//     scanf("%d%% %d%%", &a, &b);
+//     printf("%d%% %d%%", a, b);
+
+// }
+
+// limitations of datatype
+// int type take 4 byte.if we use long long it will take 8 bite.which is same for float type data.but for float its double which takes 8 byte data
+
+// 1 bit means either 0 or 1
+// 2^n - 1
+// 4 byte = 32bit that means -2^32 to 2^32 - 1 number canbe taken by integer..cause int takes 4byte
+// -10^9 to 10^9
+// long long is for 8byte = 64 bit that means -2^64 to 2^64 - 1 number canbe taken by lon long integer..cause int takes 4byte
+// -10^18 to 10^18
+// same here for float but here you have to use double and %lf for long float
+
+// #include<stdio.h>
+// int main()
+// {
+//     long long int b = 100000000000000;
+//     double c = 3.4444444444
+//     printf("%lld", b);
+//     printf("%lf", c);
+// }
 
