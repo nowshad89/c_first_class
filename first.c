ae6f139e8e6dc78c24fe944243591c273312e365
nowshad89 <nowshadmahmud89@gmail.com>
2023-04-04 22:44:07 +0600

    practice day solution one done

--- first.c
+++ first.c
@@ -134,27 +134,124 @@
 // } 
 
 // nested else if
+// #include<stdio.h>
+// int main()
+// {
+//     int taka;
+//     scanf("%d", &taka);
+//     if (taka >= 5000)
+//     {
+//         printf("cox jabo\n");
+//         if(taka >= 10000)
+//         {
+//             printf("saintmartin jabo\n");
+//         }
+//         else
+//         {
+//             printf("saint jabo na\n");
+//         }
+//     }
+//     else
+//     {
+//         printf("cox jabo na\n");
+//     }
+    
+//     return 0;
+// }
+
+// practice problem
+
+// number 1
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Recently I heard that you’ve achieved\n95%% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\tGood\nluck with all the barriers(/\\) in your life.");
+//     return 0;
+// }
+
+// number 2
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     int b;
+//     scanf("%d", &a);
+//     scanf("%d", &b);
+//     printf("%d + %d = %d\n",a, b, a+b);
+//     printf("%d - %d = %d\n",a, b, a-b);
+//     printf("%d * %d = %d\n",a, b, a*b);
+//     printf("%d / %d = %0.2f\n",a, b, a*1.0/b);
+//     return 0;
+// }
+
+// problem 3
+// You need to take one non-negative integer (0 or greater than 0) value as input and tell if it is even or odd.See the sample input and output for more clarification.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if(a % 2 == 0)
+//     {
+//         printf("even");
+//     }
+//     else
+//     {
+//         printf("odd");
+//     }
+//     return 0;
+// }
+
+// problem 4
+// You need to take one integer value as input and tell if the value is positive or negative or zero.See the sample input and output for more clarification.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if (a > 0)
+//     {
+//         printf("positive");
+//     }
+//     else if(a < 0)
+//     {
+//         printf("negative");
+//     }
+//     else{
+//         printf("zero");
+//     }
+//     return 0;
+// }
+
+//problem 5
+//  Suppose your friend has told you that she will buy a Gucci Bag if she has 10 thousand taka or more. Otherwise if she has 5 thousand taka or more, she will buy a Levis Bag. Otherwise she will buy Something from New Market. She also told you that, if she could buy the Gucci bag and she has more than 20 thousand taka she will also buy a Gucci Belt.
+
+// Now, if you know the amount of money she has, can you tell which item/items she will buy?
+// See the sample input and output for more clarification.
+
 #include<stdio.h>
 int main()
 {
-    int taka;
-    scanf("%d", &taka);
-    if (taka >= 5000)
+    int a;
+    scanf("%d", &a);
+    if(a >=10000)
     {
-        printf("cox jabo\n");
-        if(taka >= 10000)
+        printf("Gucchi Bag\n");
+        if(a > 20000)
         {
-            printf("saintmartin jabo\n");
-        }
-        else
-        {
-            printf("saint jabo na\n");
+            printf("Gucchi Belt");
         }
     }
-    else
+    else if(a >= 5000)
     {
-        printf("cox jabo na\n");
+        printf("Levis Bag");
     }
-    
+    else{
+        printf("Something");
+    }
     return 0;
 }
+
+
