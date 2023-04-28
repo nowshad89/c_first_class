ab8759f2021a3a6626fb52374fadf5201d6851b9
nowshad89 <nowshadmahmud89@gmail.com>
2023-04-29 05:26:19 +0600

    CF c compare

---
+++ CF_c_compare.c
@@ -0,0 +1,21 @@
+// Given two strings X and Y . Print the smallest lexicographical one.
+
+// Note: Lexicographical is the way of ordering the words based on the alphabetical order of their component letters.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char x[21], y[21];
+    scanf("%s %s", x, y);
+    int value = strcmp(x,y);
+    if (value <= 0)
+    {
+        printf("%s", x);
+    }
+    else
+    {
+        printf("%s", y);
+    }
+
+    return 0;
+}
